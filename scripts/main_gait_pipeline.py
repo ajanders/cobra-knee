48449c5892480197d61785eb031b7744b9de29b9
ajanders <ajanders@uw.edu>
2022-05-23 10:24:44 -0700

    updated comments to be more descriptive

--- scripts/main_gait_pipeline.py
+++ scripts/main_gait_pipeline.py
@@ -4,20 +4,29 @@ Created on Wed Feb  9 07:23:12 2022
 
 @author: Anthony Anderson
 
-This is a primary analysis pipeline for work relating to the knee flexion
-exoskeleton I've developed for transtibial amputees. The results will be
-submitted to the International Conference on Rehabilitation Robotics in
-February 2022.
+This is a primary analysis pipeline for a walking experiment I conducted with
+a knee flexion assist exoskeleton for people with transtibial amputation. This
+script produces all results for the publication titled:
+    
+"Design and Evaluation of a Knee Flexion Assistance Exoskeleton for People with
+Transtibial Amputation"
+
+by Anthony J. Anderson, Yuri F. Hudak, Kira A. Gauthier, Brittney C. Muir, and
+Patrick M. Aubin
+
+The manuscript has been accepted to the 2022 International Conference on
+Rehabilitation Robotics. 
 
 This script loads experimental data, low-pass filters signals of interest,
-segments gait cycles, and stores results in a dictionary that will be accessed
-by other scripts that make figures. This script just lays out the pipeline by
-calling functions, all of the heavy-lifting code is in the src folder.
+segments gait cycles, and stores results in a pickle file that will be accessed
+by other scripts to make figures. This script implements the analysis pipeline
+by calling functions, all of the heavy-lifting code is in the src folder.
 
 """
 
 import pickle
 
+# import my packages from the src folder
 from src import data_import as load
 from src import filters
 from src import gait_cycles
@@ -27,9 +36,15 @@ from src import results
 # %% Import all files in the specified directory
 
 """
-Data is stored in 10-second long tdms files in the 'data' folder of the
+Data is stored in 10-second long TDMS files in the 'data' folder of the
 repository. Data is not available online due to current data-sharing rules at
-the Seattle VA Hospital. Will be uploaded in the future if possible.
+the Seattle VA Hospital, but will be uploaded in the future if possible.
+
+This function loads all experimental data into a dictionary titled "data". The
+data dictionary holds raw data for each trial as a Pandas DataFrame, as well
+as metadata, file names, etc. Each subsequent function in this script modifies
+the data dictionary with new processed types of data.
+
 """
 
 participant = ['BP A 010']
@@ -49,7 +64,7 @@ filters.filter_signals(data, filter_frequencies)
 # %% Segment gait cycles
 
 # Segment the data in each file into individual strides using heel strikes
-# and normalize in time.
+# and normalize in time so that each stride has an identical number of elements
 gait_cycles.segment_strides(data)
 
 # %% Construct the average signal for each signal in each trial
@@ -57,13 +72,15 @@ gait_cycles.segment_strides(data)
 # Compute the mean stride for every signal in every file.
 averages.average_strides(data)
 
-# Compute the mean stride for every signal in every condition.
+# Compute the mean stride for every signal in every condition. I.e., each
+# experimental condition has two corresponding files. This function merges the
+# average strides across the appropriate files
 averages.condition_averages(data)
 
 # %% Compute output metrics for paper
 
-# Compute peak torque, peak cable force, average range of motion, and rms
-# tracking error over all trials
+# Compute outcomes that appear in the paper (tracking error, range of motion,
+# peak torque, etc.)
 results.compute_outcomes(data)
 
 # %% pickle data and store it
